d9098c4af4a4c1cf743ff55c1a0a12cce3a0a5ca
Diog9TV <89103529+Diog9TV@users.noreply.github.com>
2022-09-24 13:50:26 -0300

    Add files via upload

--- source/Main.hx
+++ source/Main.hx
@@ -1,6 +1,13 @@
 package;
 
-import flixel.graphics.FlxGraphic;
+#if desktop
+import Discord.DiscordClient;
+import sys.thread.Thread;
+#end
+import openfl.display.BlendMode;
+import openfl.text.TextFormat;
+import openfl.display.Application;
+import flixel.util.FlxColor;
 import flixel.FlxG;
 import flixel.FlxGame;
 import flixel.FlxState;
@@ -9,20 +16,22 @@ import openfl.Lib;
 import openfl.display.FPS;
 import openfl.display.Sprite;
 import openfl.events.Event;
-import openfl.display.StageScaleMode;
-import lime.system.System;
+
+using StringTools;
 
 class Main extends Sprite
 {
 	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
-	var initialState:Class<FlxState> = TitleState; // The FlxState the game starts with.
+	var initialState:Class<FlxState> = states.InitState; // The FlxState the game starts with.
 	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
-	var framerate:Int = 60; // How many frames per second the game should run at.
-	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
+	public static var framerate:Int = 60; // How many frames per second the game should run at.
+	#if HAXEFLIXEL_LOGO
+	var skipSplash:Bool = false;
+	#else
+	var skipSplash:Bool = true; // CRINGE! Why would you hide it????
+	#end
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
-	public static var fpsVar:FPS;
-	public static var path:String = System.applicationStorageDirectory;
 
 	// You can pretty much ignore everything from here on - your code should go in your states.
 
@@ -34,8 +43,6 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-
- 	        SUtil.gameCrashCheck();
 
 		if (stage != null)
 		{
@@ -71,26 +78,41 @@ class Main extends Sprite
 			gameHeight = Math.ceil(stageHeight / zoom);
 		}
 
-		SUtil.doTheCheck();
-
-		#if !debug
-		initialState = TitleState;
-		#end
-	
-		ClientPrefs.loadDefaultKeys();
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
-		fpsVar = new FPS(10, 3, 0xFFFFFF);
-		addChild(fpsVar);
-		Lib.current.stage.align = "tl";
-		Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
-		if(fpsVar != null) {
-			fpsVar.visible = ClientPrefs.showFPS;
-		}
-
-		#if html5
-		FlxG.autoPause = false;
-		FlxG.mouse.visible = false;
+		#if !mobile
+		addChild(new ui.FPSMem(10, 3, 0xFFFFFF));
 		#end
+	}
+
+	public static function setFPSCap(cap:Int)
+	{
+		Main.framerate=cap;
+		updateFramerate();
+	}
+
+	// thank u forever engine
+	// https://github.com/Yoshubs/Forever-Engine/blob/master/source/Main.hx
+
+	public static function updateFramerate(){
+		if (Main.framerate > FlxG.updateFramerate)
+		{
+			FlxG.updateFramerate = Main.framerate;
+			FlxG.drawFramerate = Main.framerate;
+		}
+		else
+		{
+			FlxG.drawFramerate = Main.framerate;
+			FlxG.updateFramerate = Main.framerate;
+		}
+	}
+
+	public static function adjustFPS(num:Float):Float{
+		return num * (60/Main.getFPSCap());
+	}
+
+	public static function getFPSCap():Float
+	{
+		return FlxG.drawFramerate;
 	}
 }
